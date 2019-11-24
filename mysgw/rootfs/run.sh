6e297f9a87888c9f718a0d9d0b740234bc9d28aa
tiegs <github@tilmaneggers.de>
2019-11-24 23:55:44 +0100

    Update run.sh

--- mysgw/rootfs/run.sh
+++ mysgw/rootfs/run.sh
@@ -1,12 +1,12 @@
 #!/bin/ash
 
 echo "MySensors Gateway"
-MYSGW_TYPE=$(jq "${CONFIG_PATH}" ".type")
-MYSGW_TRN=$(jq "${CONFIG_PATH}" ".transport")
-MQTT_SERVER=$(jq "${CONFIG_PATH}" ".mqtt_server")
-MQTT_CLIENTID=$(jq "${CONFIG_PATH}" ".mqtt_clientid")
-MQTT_TOPIC_IN=$(jq "${CONFIG_PATH}" ".mqtt_topicin")
-MQTT_TOPIC_OUT=$(jq "${CONFIG_PATH}" ".mqtt_topicout")
+MYSGW_TYPE=$(jq ".type" "${CONFIG_PATH}")
+MYSGW_TRN=$(jq ".transport" "${CONFIG_PATH}")
+MQTT_SERVER=$(jq ".mqtt_server" "${CONFIG_PATH}")
+MQTT_CLIENTID=$(jq ".mqtt_clientid" "${CONFIG_PATH}")
+MQTT_TOPIC_IN=$(jq ".mqtt_topicin" "${CONFIG_PATH}")
+MQTT_TOPIC_OUT=$(jq ".mqtt_topicout" "${CONFIG_PATH}")
 
 MQTT_OPTS="--my-mqtt-client-id=$MQTT_CLIENTID --my-controller-url-address=$MQTT_SERVER --my-mqtt-publish-topic-prefix=$MQTT_TOPIC_OUT --my-mqtt-subscribe-topic-prefix=$MQTT_TOPIC_IN"
 
